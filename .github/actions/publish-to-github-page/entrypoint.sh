b0ad05a018aec5435c3dac33c0a03d566289aec1
Myon <myon.cn@gmail.com>
2019-08-15 17:21:44 +0800

    push test

---
+++ .github/actions/publish-to-github-page/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# check values
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "error: not found GITHUB_TOKEN"
+    exit 1
+fi
+
+# initialize git
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config user.name "Automated Publisher"
+git config user.email "publish-to-github-action@users.noreply.github.com"
+git remote add publisher "${remote_repo}"
+git show-ref # useful for debugging
+git branch --verbose
+
+# publish any new files
+git add -A
+timestamp=$(date -u)
+git commit -m "Automated publish: ${timestamp} ${GITHUB_SHA}"
+git branch tmp
+#git checkout master
+#git merge tmp
+git push publisher tmp
